e93babab40953fc92858eb63e4a13749597017f7
Frans van Ek <FransvanEk@Innax.nl>
2021-11-03 19:13:28 +0100

    created json editor adn json diff components

---
+++ Client/Shared/Components/JsonDiff.razor.cs
@@ -0,0 +1,56 @@
+﻿using BlazorMonaco;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JLioOnline.Client.Shared.Components
+{
+    public partial class JsonDiff : ComponentBase
+    {
+        private MonacoDiffEditor diffEditor { get; set; }
+
+        [Parameter]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Parameter]
+        public string OriginalJson { get; set; }
+
+        [Parameter]
+        public EventCallback<string> OriginalJsonChanged { get; set; }
+
+        [Parameter]
+        public string ModifiedJson { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ModifiedJsonChanged { get; set; }
+
+        private DiffEditorConstructionOptions DiffEditorConstructionOptions(MonacoDiffEditor editor)
+        {
+            return new DiffEditorConstructionOptions
+            {
+                OriginalEditable = false,
+                InDiffEditor = false
+            };
+        }
+
+        public async Task Update()
+        {
+            await DiffEditorInit(diffEditor);
+        }
+
+        private async Task DiffEditorInit(MonacoEditorBase editor)
+        {
+            var original_model = await MonacoEditorBase.CreateModel(OriginalJson, "json");
+            var modified_model = await MonacoEditorBase.CreateModel(ModifiedJson, "json");
+
+            await diffEditor.SetModel(new DiffEditorModel
+            {
+                Original = original_model,
+                Modified = modified_model
+            });
+        }
+
+    }
+}
